ade2ee0b893f78cab67e4ea9f9c1917f67c891e8
Lucas Ingaruca <lucaspe123321@gmail.com>
2025-10-06 11:15:39 -0500

    agregando PIN- prueba 2/agregue dominio+sertificacion https para el servidor del backend

--- js/Adm.js
+++ js/Adm.js
@@ -3,6 +3,7 @@ const API_URL = "https://laimserver.duckdns.org/api";
 fetch('https://laimserver.duckdns.org/api/ValidarSesion', { credentials: 'include' })
   .then(res => res.json())
   .then(data => {
+    console.log("ValidarSesion:", data);
     if (!data.autenticado) {
       window.location.href = "../index.html";
     }
